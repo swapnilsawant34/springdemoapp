9fa2a81d106d745d6939a0c7d647bafe16a06a9e
Swapnil <29swapnilsawant@gmail.com>
2024-05-15 18:18:11 +0530

    demo_spring_app

---
+++ src/main/java/com/example/demo/ctrl/TestCtrl.java
@@ -0,0 +1,13 @@
+package com.example.demo.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestCtrl {
+    @GetMapping("/welcome")
+    public String home() {
+		return "welcome in Spring boot application";
+    	
+    }
+}

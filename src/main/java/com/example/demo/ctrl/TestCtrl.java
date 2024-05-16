d3ca9efe8a40bf6610505a8000cb08c695e01840
Swapnil <29swapnilsawant@gmail.com>
2024-05-16 15:56:04 +0530

    spring_demo1

--- src/main/java/com/example/demo/ctrl/TestCtrl.java
+++ src/main/java/com/example/demo/ctrl/TestCtrl.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestCtrl {
     @GetMapping("/welcome")
     public String home() {
-		return "welcome in Spring boot application";
+		return "welcome to Spring boot application";
     	
     }
 }
